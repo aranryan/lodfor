310ff7dd7715b2d0ebaaf5bb151c812601679a1a
Aran Ryan <aran.ryan@gmail.com>
2015-07-26 21:30:03 -0400

    renaming, numeric first

--- scripts/main.R
+++ scripts/main.R
@@ -12,27 +12,27 @@ source("scripts/functions.R")
 
 # read raw STR monthly files
 # results saved in Rdata file (dataframe)
-source("scripts/read_raw_str_us.R")
-source("scripts/read_raw_str_ihg_mex.R")
-source("scripts/read_raw_str_ihg_can.R")
-source("scripts/read_raw_str_us_host.R")
+source("scripts/010_read_raw_str_us.R")
+source("scripts/011_read_raw_str_ihg_mex.R")
+source("scripts/012_read_raw_str_ihg_can.R")
+source("scripts/013_read_raw_str_us_host.R")
 
 # create monthl and quarterly seasonal factors, only run if necessary
 # results saved in Rdata files (dataframe)
-#source("scripts/create_seasonal.R")
+#source("scripts/014_create_seasonal.R")
 
 # preps the STR data, including creating seasonal adjusted from factors
 # results saved in Rdata (xts) and csv files 
-source("scripts/prep_str.R")
-source("scripts/prep_str_host.R")
+source("scripts/020_prep_str.R")
+source("scripts/021_prep_str_host.R")
 
 # reads STR opens and closes directly from Excel source file
 # results saved in Rdata (xts) and csv files
-source("scripts/load_str_openclose.R")
+source("scripts/030_load_str_openclose.R")
 
 # reads macro source file
 # results saved in Rdata (xts) and csv files
-rmarkdown::render('scripts/load_usmacro.Rmd')
+rmarkdown::render('scripts/040_load_usmacro.Rmd')
 #knit("scripts/load_usmacro.Rmd")
 
 # creates the ushist file
@@ -41,23 +41,26 @@ rmarkdown::render('scripts/load_usmacro.Rmd')
 # unit it using knit. I'm not sure why not. Could search
 # for examples of people maintaining code in Rmd format
 # maybe they don't
-knit("scripts/create_ushist.Rmd")
+knit("scripts/050_create_ushist.Rmd")
+
+# do I need this one? does it even work?
+#knit("scripts/050_create_ushist.Rmd")
 
 # runs simple forecast 
-source("scripts/simple_forecast.R")
+source("scripts/060_simple_forecast.R")
 
 # reads in the current forecast 
-source("scripts/read_usfor.R")
+source("scripts/070_read_usfor.R")
 
 # creates a Rdata file with Fred data used in graphs
-source("scripts/pull_fred_data.R")
+source("scripts/080_pull_fred_data.R")
 
 # compiles some groups of top 25 markets
-source("scripts/compile_top25.R")
+source("scripts/090_compile_top25.R")
 
 # runs the markdown file
-rmarkdown::render("scripts/us_overview_graphs.Rmd") 
+rmarkdown::render("scripts/100_us_overview_graphs.Rmd") 
 
 # creates an eviews output for host analysis
-source("scripts/out_to_eviews.R")
+source("scripts/110_out_to_eviews.R")
 
