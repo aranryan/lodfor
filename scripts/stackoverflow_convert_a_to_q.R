d1f00a7358b8889a4bbd4963c989c1b7a5018638
Aran Ryan <aran.ryan@gmail.com>
2016-05-25 17:08:19 -0400

    test answer!

--- scripts/stackoverflow_convert_a_to_q.R
+++ scripts/stackoverflow_convert_a_to_q.R
@@ -1,4 +1,8 @@
 library(zoo)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(tempdisagg)
 
 # create annual example series
 a <- as.numeric(c("100", "110", "111"))
@@ -31,7 +35,30 @@ ggplot(data=dat, aes(x=date, y=value, group=variable, color=variable)) +
   geom_point(data = subset(dat,variable == "z_a"),  colour="red", shape=1, size=7)
 
 
+###########
 
+z_a <- ts(c(100, 110, 111), start = 2000)
+z_a
+z_q <- predict(td(z_a ~ 1, method = "denton-cholette", conversion = "average"))
+z_q
+
+z_a <- as.zoo(z_a)
+z_q <- as.zoo(z_q)
+c_1 <- merge(z_a, z_q); c_1
+
+
+tapply(z_q, floor(time(z_q)), mean)
+
+dat <- c_1 %>%
+  data.frame(date=time(.), .) %>%
+  gather(variable, value, -date)
+ggplot(data=dat, aes(x=date, y=value, group=variable, color=variable)) +
+  geom_line() +
+  geom_point() +
+  theme(legend.position=c(.7, .4)) + 
+  geom_point(data = subset(dat,variable == "z_a"),  colour="red", shape=1, size=7)
+
+############
 
 c$z_q_amean <- ave(coredata(c$z_q), yr, FUN = function(x) mean(x))
 c$z_q_a <- ave(coredata(c$z_q), yr, FUN = function(x) x[1])
